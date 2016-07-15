1ad80c1c7504c3115791a06cce751d9799ec9358
Daniel Kazemian <Dkazem91@users.noreply.github.com>
2016-07-14 23:04:20 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var itemnumber = 5;
+  var randomactive = Math.floor(Math.random() * 9);
+  var color = ['#F98866', '#FF420E', '#80BD9E', '#89DA59', '#90AFC5', '#336B87', '#2A3132', '#763626'];
+  var randomcolor = color[Math.floor(Math.random() * color.length)];
+  $('body, .container, p').fadeIn('slow');
+  $('body').css('background-color', randomcolor);
+  $('footer').css('color', randomcolor);
+  $('#myCarousel').css('color', randomcolor);
+  $('button').css('background-color', randomcolor);
+  $("p").prepend("<i class='fa fa-quote-left' style='font-size:30px' aria-hidden='true'></i>      ");
+
+  $('.carousel .item').eq(randomactive).addClass('active');
+  $('a').on('click', function() {
+    randomcolor = color[Math.floor(Math.random() * color.length)];
+    $('.item').animate({
+      opacity: 0
+    }, 500);
+
+    $('body').css('background-color', randomcolor);
+    $('footer').css('color', randomcolor);
+    $('#myCarousel').css('color', randomcolor);
+    $('button').css('background-color', randomcolor);
+  });
+
+  $('a').on('click', function(event) {
+    var duration = 700;
+    setTimeout(function() {
+      $('#myCarousel').carousel(itemnumber);
+      var keeprandom = Math.floor(Math.random() * 9);
+      itemnumber = (keeprandom === itemnumber) ? itemnumber + 1 : keeprandom;
+      $('.item').animate({
+        opacity: 1
+      }, 500);
+    }, duration);
+
+  });
+});
